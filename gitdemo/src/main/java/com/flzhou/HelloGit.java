8557e9f1c23bbf98ede18ae3bec581114315b57a
fanlin_zhou <7437676+fanlin_zhou@user.noreply.gitee.com>
2020-09-07 16:08:42 +0800

    第三版本的git

--- gitdemo/src/main/java/com/flzhou/HelloGit.java
+++ gitdemo/src/main/java/com/flzhou/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("第一个git");
         System.out.println("第二个git");
+        System.out.println("创建分支之后提交git到新建的dev分支");
     }
 }
